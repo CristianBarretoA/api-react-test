513a6f8cb9ee58e223d4386c842c23ba03b35c7f
Cristian Barreto <cristian.savage@outlook.com>
2020-05-21 01:47:38 -0500

    Ajustes para despliegue a produccion

--- src/index.js
+++ src/index.js
@@ -4,17 +4,11 @@ import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import App from "./components/App";
-import Productos from "./components/productos/Productos"
-
-
-//const container = document.getElementById("root")
-//ReactDOM.render(<Productos />, container );
 
 ReactDOM.render(
     <Router>
         <div>
             <Switch>
-                <Route path="/productos" component={Productos}/>
                 <Route path="/" component={App} exact/>
             </Switch>
         </div>
