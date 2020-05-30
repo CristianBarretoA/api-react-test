d9d06441244caddfcdfd4cec7cf7393d96d4d78d
Cristian Barreto <cristian.savage@outlook.com>
2020-05-30 11:32:05 -0500

    Se agregar configuracion para desplegar aplciativo en firebase

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 
 import App from "./components/App";
 
-
-const container = document.getElementById("root")
-ReactDOM.render(<App />, container );
+const container = document.getElementById("root");
+ReactDOM.render(<App />, container);
 
 /*
 ReactDOM.render(
